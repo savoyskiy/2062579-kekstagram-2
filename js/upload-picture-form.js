bca457e3ffb94ef1f5a4e933549af3d09181a752
savoyskiy <savoyskiy83@yandex.ru>
2024-09-21 09:33:29 +0300

    fix: вносит корректировки по код-ревью

--- js/upload-picture-form.js
+++ js/upload-picture-form.js
@@ -37,13 +37,9 @@ const closePictureUploadForm = () => {
 
 // закрываю форму клавишей Esc
 const onEscapeKeyDown = (evt) => {
-  // проверяю, что фокус не на полях ввода хэштегов и комментария
-  // if (isEscapeKey(evt) && !evt.target.classList.contains('text__hashtags') && !evt.target.classList.contains('text__description')) {
-  //   evt.preventDefault();
-  //   closePictureUploadForm();
-  // }
   if(isEscapeKey(evt)) {
     evt.preventDefault();
+    // проверяю, что фокус на полях ввода хэштегов и комментария
     if(document.activeElement === inputHashtagsElement || document.activeElement === textCommentElement) {
       evt.stopPropagation();
     } else {
