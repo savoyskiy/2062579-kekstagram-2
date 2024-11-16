71588587d3aab272c72c8cc5357356e5964af7b5
savoyskiy <savoyskiy83@yandex.ru>
2024-11-16 21:22:15 +0300

    feat: перерабатывает модуль работы с сервером, безуспешно пытается отобразить ошибку при загрузке =/

--- js/communication-server.js
+++ js/communication-server.js
@@ -1,7 +1,49 @@
-const getServerData = async () => {
-  const response = await fetch('https://31.javascript.htmlacademy.pro/kekstagram/data');
-  const data = await response.json();
-  return data;
+// адрес для получения данных с сервера
+const serverUrlGetData = 'https://31.javascript.htmlacademy.pro/kekstagram/data';
+// адрес для отправки данных на сервер
+const serverUrlPost = 'https://31.javascript.htmlacademy.pro/kekstagram/';
+// методы
+const methodGet = 'GET';
+const methodPost = 'POST';
+// шаблон сообщения об ошибке загрузки данных
+const errorLoadDataTemplate = document.querySelector('#data-error').textContent;
+const body = document.body;
+// время отображения сообщения об ошибке
+const REMOVE_ERROR_MESSAGE_TIMEOUT = 5000;
+
+// функция отображения сообщения об ошибке
+const showErrorMessage = () => {
+  // клонирую элемент по шаблону
+  const errorLoadDataElement = errorLoadDataTemplate.cloneNode(true);
+  // добавляю клонированный элемент на страницу
+  body.append(errorLoadDataElement);
+  // нахожу этот элемент
+  const errorMessageElement = document.querySelector('.data-error');
+  // удаляю его через установленное время
+  setTimeout(() => {
+    errorMessageElement.remove();
+  }, REMOVE_ERROR_MESSAGE_TIMEOUT);
+};
+// функция обращения к серверу
+const loader = async (address, method, requestBody) => {
+  const response = await fetch(address, {method, requestBody});
+  return response.ok ? await response.json() : Promise.reject();
+};
+// функция получения данных с сервера
+const getServerData = () => loader(serverUrlGetData, methodGet, null);
+// функция отправки данных на сервер
+const postServerData = (requestBody) => loader(serverUrlPost, methodPost, requestBody);
+
+const bootstrap = async () => {
+  let pictures = [];
+  try {
+    pictures = await getServerData();
+    return pictures;
+  } catch (error) {
+    showErrorMessage(error);
+  }
 };
 
-export {getServerData};
+bootstrap();
+
+export {getServerData, postServerData};
